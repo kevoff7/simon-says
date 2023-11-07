014e8e5db2f4b880b3df249008437f8f5fd06e49
kevoff <suviacruzfer@gmail.com>
2023-11-06 21:56:47 -0300

    version 1.0 completed

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let userSequence = []
+let computerSequence = []
+let isGameStarted = false
+
 function removeClassBox(boxElement){
     boxElement.classList.remove('opacity-100')
 }
@@ -9,30 +13,123 @@ function addClassBox(boxElement){
         removeClassBox(boxElement)
     }, transitionDuration)
 }
-  
 
-function showComputerSequence(boxElements,sequence){
+function changeUiButton(isGameStarted){
+    const $buttonStart = document.querySelector('#button-start')
+    if(isGameStarted){
+        $buttonStart.textContent = 'Reset Game'
+        $buttonStart.className = 'btn btn-danger'
+    }
+    else{
+        $buttonStart.textContent = 'Start Game'
+        $buttonStart.className = 'btn btn-primary'    
+    }
+}
+
+function changeUiSubtitle(value){
+    const $subtitleText = document.querySelector('#subtitle-text')
+    $subtitleText.textContent = value
+}
+
+function changeUiRoundGame(){
+    const $roundGame = document.querySelector('#round-game')
+    $roundGame.textContent = `Round # - ${computerSequence.length}`
+}
+
+function changeUI(isGame, valueSubtitle){
+    changeUiButton(isGame)
+    changeUiSubtitle(valueSubtitle);
+    changeUiRoundGame()
+}
+
+function resetGame(){
+    userSequence = []
+    computerSequence = []
+    isGameStarted = false
+    blockUserInput()
+    changeUI(isGameStarted, 'Tap "Start Playing" to play!')
+   
+}
+
+function lose(){
+    blockUserInput()
+    changeUiSubtitle('You lost, Tap "Reset Game"!')
+    userSequence = []
+}
+
+function checkSequence(){
+    for(let i = 0; i < userSequence.length; i++){
+        if(userSequence[i] !== computerSequence[i]){
+            lose()
+        }
+    }
+}
+
+function showComputerSequence(boxElements){
     const delayBetweenBoxes = 1000
-    for (let i = 0; i < sequence.length; i++) {
+    for (let i = 0; i < computerSequence.length; i++) {
         setTimeout(() => {
-            addClassBox(boxElements[sequence[i]])
+            addClassBox(boxElements[computerSequence[i]])
         }, i * delayBetweenBoxes)
     }
 }
 
-function addRandomBox(sequence, maxIndex){
+function addRandomBox(maxIndex){
     const randomBox = Math.floor(Math.random() * maxIndex)
-    sequence.push(randomBox)
+    computerSequence.push(randomBox)
+}
+
+function gameInicialized() {
+    const $boxElements = document.querySelectorAll('.box')
+    isGameStarted = true
+    blockUserInput()
+    addRandomBox($boxElements.length)
+    showComputerSequence($boxElements)
+    changeUI(isGameStarted, 'Computer turn')
+    
+    setTimeout(() => {
+        unblockInputUser()
+        changeUiSubtitle('User turn')
+    }, computerSequence.length * 1000);
+}
+
+function userTurn($box,count){
+    if(userSequence.length < computerSequence.length){ 
+        userSequence.push(count)
+        addClassBox($box)
+        checkSequence()
+        if(userSequence.length === computerSequence.length){
+              setTimeout(() => {
+                userSequence = []
+                gameInicialized()
+            }, 1000)
+        }
+    }
+}
+
+function unblockInputUser() {
+    document.querySelectorAll('.box').forEach(($box,i) => {
+      $box.onclick = (() => {
+          userTurn($box,i);
+      })
+    });
+}
+
+function blockUserInput() {
+    document.querySelectorAll('.box').forEach(($box) => {
+      $box.onclick = () => {
+        };
+    });
 }
 
 function startGame(){
-    const $buttonStart = document.querySelector('#button-start')
-    const $boxElements = document.querySelectorAll('.box')
-
-    const computerSequence = []
+    const $buttonStart = document.querySelector('#button-start')    
     $buttonStart.addEventListener('click', () => {
-            addRandomBox(computerSequence,$boxElements.length)
-            showComputerSequence($boxElements,computerSequence)
+        if(!isGameStarted){
+            gameInicialized()
+        }else{
+            resetGame()
+        }
     })
 }
 
